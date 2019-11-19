70f2df3b53fd71406a3d11306aadd9aab0a9ee9e
david <davidfr333@gmail.com>
2019-11-19 18:06:32 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+A Ross Node that publishes to a topic and then reads from that Topic
---
+++ src/dom/domSender.cpp
@@ -0,0 +1,23 @@
+//lets hope this works
+
+#include <ros/ros.h>
+#include "std_msgs/String.h"
+int main(int argc, char **argv) {
+ros::init(argc, argv, "publish_selfmade");
+ros::NodeHandle nh;
+ros::Publisher pub = nh.advertise<std_msgs::String>("chatter", 1000);
+ros::Rate loop_rate(10);
+int count = 0;
+while (ros::ok()){
+	std_msgs::String msg;
+	msg.data = "Hello From Another World";
+	
+	pub.publish(msg);
+	ros::spinOnce();
+	loop_rate.sleep();
+	++count;
+
+}
+
+return 0;
+}
